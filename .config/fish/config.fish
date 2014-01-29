783614174188b00b56bce43d275dd57643cc78e2
Rafael <nvieirarafael@gmail.com>
2014-01-29 14:26:03 -0200

    added ssh alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,13 @@
+# APPLICATIONS
 alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+
+alias corona_terminal_old='/Applications/CoronaSDK_old/Corona\ Terminal'
 alias corona_terminal='/Applications/CoronaSDK/Corona\ Terminal'
+
+# SCRIPTS
+alias cf='coffee -cw -o js/ coffee/'
+alias cm='compass watch --debug-info'
+
+# SSH
+alias sshwemove='ssh evrm_admin@ftp.wemoveapp.com'
+alias sshew='ssh rafael@everywherekadow.no-ip.org'
