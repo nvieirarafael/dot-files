15ff89f6fbfb74589e9daf186bc76ed4aca9e2f2
Rafael <nvieirarafael@gmail.com>
2014-02-21 15:19:34 -0300

    change on config.fish and added .bash_profile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+
 # APPLICATIONS
 alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 
@@ -6,8 +7,16 @@ alias corona_terminal='/Applications/CoronaSDK/Corona\ Terminal'
 
 # SCRIPTS
 alias cf='coffee -cw -o js/ coffee/'
-alias cm='compass watch --debug-info'
+alias cm='compass watch'
+alias cm_debug='compass watch --debug-info'
 
 # SSH
 alias sshwemove='ssh evrm_admin@ftp.wemoveapp.com'
 alias sshew='ssh rafael@everywherekadow.no-ip.org'
+
+# JAVA ENV
+setenv JAVA_HOME (/usr/libexec/java_home)
+setenv J2EE_HOME /Applications/NetBeans/glassfish-4.0/glassfish
+
+# GLASSFISH
+alias asadmin='/Applications/NetBeans/glassfish-4.0/glassfish/bin/asadmin'
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ dot-files
 ##### Criar link para o fish
 * ln -s ~/workspace/dot-files/.config/fish/ ~/.config/fish
 
+##### Criar link para o bash_profile (ainda não testei)
+* ln -s ~/workspace/dot-files/.config/.bash_profile ~/.config/.bash_profile
+
