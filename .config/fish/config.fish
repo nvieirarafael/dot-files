77e4e17de3b1abef77ea4b25d021fec4ffd0492e
Rafael Nunes Vieira <rafael.vieira@ima.sp.gov.br>
2015-05-07 15:04:52 -0300

    ajustes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 # IMA PROXY CONFIGS
 set -gx http_proxy http://172.21.0.161:3128
+set -gx HTTP_PROXY $http_proxy
 set -gx https_proxy $http_proxy
+set -gx HTTPS_PROXY $http_proxy
 set -gx ftp_proxy $http_proxy
 set -gx rsync_proxy $http_proxy
 set -gx no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
